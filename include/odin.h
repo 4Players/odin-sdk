1e2004e809e9026c08268f04305b41148800caa5
Sven Paulsen <info@sven-paulsen.de>
2025-05-20 17:18:13 +0200

    update to version 1.7.1

--- include/odin.h
+++ include/odin.h
@@ -10,7 +10,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define ODIN_VERSION "1.7.0"
+#define ODIN_VERSION "1.7.1"
 
 /**
  * Available versions of the automatic gain controller (AGC) to be used. This adjusts the audio
